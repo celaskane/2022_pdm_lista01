747949154b922fb51557c29fcf532b894ef6c2c1
celaskane <leandoru9@gmail.com>
2022-04-05 18:45:54 -0300

    Iniciando desenvolvimento da lista

---
+++ src/ex01.js
@@ -0,0 +1,3 @@
+/*1. Escreva funções que implementam as quatro operações aritméticas. Todas elas devem
+receber dois valores numéricos como parâmetro e devolver o valor de sua respectiva
+operação. Faça a implementação usando a palavra function para todas elas.*/
